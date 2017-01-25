b6492d61ae5351bc8c8904a55d81d46ab3d24052
ThunderAnimal <MartinWeber.93@outlook.com>
2017-01-25 01:29:11 +0100

    fix Detection with own classify

--- ClassificationDetection/ClassificationDetection.cs
+++ ClassificationDetection/ClassificationDetection.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
@@ -8,7 +9,6 @@ using ClassificatioDataGenerator;
 using weka.classifiers;
 using weka.core;
 using Glaukopis.Adapters.PicNetML;
-
 
 namespace ClassificationDetection
 {
@@ -43,7 +43,7 @@ namespace ClassificationDetection
                 using (var slideCache = new SlideCache(slideName))
                 {
                     //Compute Class
-                    var tissueSlidePartitioner = new SlidePartitioner<TissuAnnotaionEnum>(slideCache.Slide, 1f,
+                    var tissueSlidePartitioner = new SlidePartitioner<Dictionary<string, TissuAnnotaionEnum>>(slideCache.Slide, 1f,
                         new Size(300, 300));
                     ComputeClass(tissueSlidePartitioner, slideCache);
 
@@ -63,110 +63,158 @@ namespace ClassificationDetection
             Console.ReadKey();
         }
 
-        private static void ComputeClass(SlidePartitioner<TissuAnnotaionEnum> tissueSlidePartitioner,
+        private static void ComputeClass(SlidePartitioner<Dictionary<string, TissuAnnotaionEnum>> tissueSlidePartitioner,
             SlideCache slideCache)
         {
             Parallel.ForEach(tissueSlidePartitioner.Values, (tile) =>
             {
                 using (var tileImage = slideCache.Slide.GetImagePart(tile))
                 {
-                    var classStr = "";
-                    if (!ComputeImageFeatures.IsTissue(tileImage))
-                    {
-                        tile.Data = TissuAnnotaionEnum.NoTissue;
-                        classStr = "NoTissue";
-                    }
-                    else
-                    {
-                        var tissueAnnotation = new TissueAnnotationClass();
-                        tissueAnnotation = tissueAnnotation.ComputeFeatureValues(tileImage);
-                        classStr = ClassifierExtensions.Classify(_classifier, _dataSet,
-                            tissueAnnotation.GetFeatures());
-                        switch (classStr)
-                        {
-                            case "Fettgewebe": tile.Data = TissuAnnotaionEnum.Fettgewebe;
-                                break;
-                            case "Tumor":
-                                tile.Data = TissuAnnotaionEnum.Tumor;
-                                break;
-                            case "Entzündung":
-                                tile.Data = TissuAnnotaionEnum.Entzuendung;
-                                break;
-                            case "Gefäß":
-                                tile.Data = TissuAnnotaionEnum.Gefaess;
-                                break;
-                            case "Mikrokalk":
-                                tile.Data = TissuAnnotaionEnum.Mikrokalk;
-                                break;
-                            case "Nerv":
-                                tile.Data = TissuAnnotaionEnum.Nerv;
-                                break;
-                            case "Stroma":
-                                tile.Data = TissuAnnotaionEnum.Stroma;
-                                break;
-                            case "DCIC":
-                                tile.Data = TissuAnnotaionEnum.DCIC;
-                                break;
-                            case "Kalk":
-                                tile.Data = TissuAnnotaionEnum.Kalk;
-                                break;
-                            case "Normales Mammaepithel":
-                                tile.Data = TissuAnnotaionEnum.NormalesMammaepithel;
-                                break;
-                            default: tile.Data = TissuAnnotaionEnum.UnknowClass;
-                                break;
-                        }
-                    }
-                    Console.WriteLine(slideCache.SlideName + "-" + tile.Index + " done - Class: " + classStr);
+                    var tissueAnnotaion = ComputeFeatues(tileImage);
+                    var classifyDic = new Dictionary<string, TissuAnnotaionEnum>();
+
+                    classifyDic.Add("ownClassificationJ48", ClassificationOwn.ClassifyJ48(tissueAnnotaion));
+                    classifyDic.Add("ownClassifyJRip", ClassificationOwn.ClassifyJRip(tissueAnnotaion));
+
+                    //Funktioiert leider nicht, immer leer ""
+                    classifyDic.Add("wekaClassifyBagging",ClassifyWeka(tissueAnnotaion, _classifier, _dataSet));
+
+                    tile.Data = classifyDic;
+                    Console.WriteLine(slideCache.SlideName + "-" + tile.Index + " done - Class: " + showOutput(tile.Data["ownClassificationJ48"]));
                 }
             });
         }
 
-        private static void DrawHeatMapAll(SlidePartitioner<TissuAnnotaionEnum> tissueSlidePartitioner, SlideCache slideCache)
+        private static TissueAnnotationClass ComputeFeatues(Bitmap image)
         {
-            Func<TissuAnnotaionEnum, Color> f = annotaion =>
+            if (!ComputeImageFeatures.IsTissue(image))
             {
-                switch (annotaion)
-                {
-                    case TissuAnnotaionEnum.Fettgewebe:
-                        return Color.White;
-                    case TissuAnnotaionEnum.Tumor:
-                        return Color.Red;
-                    case TissuAnnotaionEnum.Entzuendung:
-                        return Color.Aqua;
-                    case TissuAnnotaionEnum.Gefaess:
-                        return Color.Green;
-                    case TissuAnnotaionEnum.Mikrokalk:
-                        return Color.BlueViolet;
-                    case TissuAnnotaionEnum.Nerv:
-                        return Color.SaddleBrown;
-                    case TissuAnnotaionEnum.Stroma:
-                        return Color.Yellow;
-                    case TissuAnnotaionEnum.DCIC:
-                        return Color.SpringGreen;
-                    case TissuAnnotaionEnum.Kalk:
-                        return Color.Tan;
-                    case TissuAnnotaionEnum.NormalesMammaepithel:
-                        return Color.DarkOrange;
-                    case TissuAnnotaionEnum.NoTissue:
-                        return Color.Gray;
-                    case TissuAnnotaionEnum.UnknowClass:
-                        return Color.Black;
-                    default:
-                        return Color.Black;
-                }
-            };
-            using (var heatMap = tissueSlidePartitioner.GenerateHeatMap(f))
+                return null;
+            }
+            else
             {
-                slideCache.SetImage("ClassificationDetection", heatMap);
+                var tissueAnnotation = new TissueAnnotationClass();
+                tissueAnnotation = tissueAnnotation.ComputeFeatureValues(image);
+                return tissueAnnotation;
             }
         }
 
-        private static void DrawHeatMapOneClass(SlidePartitioner<TissuAnnotaionEnum> tissueSlidePartitioner, SlideCache slideCache, TissuAnnotaionEnum annotaion)
+        private static TissuAnnotaionEnum ClassifyWeka(TissueAnnotationClass tissueAnnotation, Classifier classifier, Instances dataSet)
+        {
+            if (tissueAnnotation == null)
+            {
+                return TissuAnnotaionEnum.NoTissue;
+            }
+            if (tissueAnnotation.GetFeatures() == null)
+            {
+                return TissuAnnotaionEnum.UnknowClass;
+            }
+            switch (ClassifierExtensions.Classify(classifier, dataSet,
+                            tissueAnnotation.GetFeatures()))
+            {
+                case "Fettgewebe": return TissuAnnotaionEnum.Fettgewebe;
+                case "Tumor": return TissuAnnotaionEnum.Tumor;
+                case "Entzündung": return TissuAnnotaionEnum.Entzuendung;
+                case "Gefäß": return TissuAnnotaionEnum.Gefaess;
+                case "Mikrokalk": return TissuAnnotaionEnum.Mikrokalk;
+                case "Nerv": return TissuAnnotaionEnum.Nerv;
+                case "Stroma": return TissuAnnotaionEnum.Stroma;
+                case "DCIC": return TissuAnnotaionEnum.DCIC;
+                case "Kalk": return TissuAnnotaionEnum.Kalk;
+                case "Normales Mammaepithel": return TissuAnnotaionEnum.NormalesMammaepithel;
+                default: return TissuAnnotaionEnum.UnknowClass;
+            }
+        }
+
+        private static String showOutput(TissuAnnotaionEnum annotaion)
+        {
+            switch (annotaion)
+            {
+                case TissuAnnotaionEnum.Fettgewebe:
+                    return "Fettgewebe";
+                case TissuAnnotaionEnum.Tumor:
+                    return "Tumor";
+                case TissuAnnotaionEnum.Entzuendung:
+                    return "Entzündung";
+                case TissuAnnotaionEnum.Gefaess:
+                    return "Gefäß";
+                case TissuAnnotaionEnum.Mikrokalk:
+                    return "Mikrokalk";
+                case TissuAnnotaionEnum.Nerv:
+                    return "Nerv";
+                case TissuAnnotaionEnum.Stroma:
+                    return "Stroma";
+                case TissuAnnotaionEnum.DCIC:
+                    return "DCIC";
+                case TissuAnnotaionEnum.Kalk:
+                    return "Kalk";
+                case TissuAnnotaionEnum.NormalesMammaepithel:
+                    return "Normales Mammaepithel";
+                case TissuAnnotaionEnum.NoTissue:
+                    return "Kein Gewebe";
+                case TissuAnnotaionEnum.UnknowClass:
+                    return "Keine Zuordnung";
+                default:
+                    return "Keine Zuordnung";
+            }
+        }
+
+        private static void DrawHeatMapAll(SlidePartitioner<Dictionary<String, TissuAnnotaionEnum>> tissueSlidePartitioner, SlideCache slideCache)
+        {
+            Func<Dictionary<String, TissuAnnotaionEnum>, Color> drawWekaClassificationBaggingFunc = classifyDic => TissuAnnotaionToColor(classifyDic["wekaClassifyBagging"]);
+            Func< Dictionary < String, TissuAnnotaionEnum >, Color > drawOwnClassificationJ48Func = classifyDic => TissuAnnotaionToColor(classifyDic["ownClassificationJ48"]);
+            Func<Dictionary<String, TissuAnnotaionEnum>, Color> drawOwnClassificationJRipFunc = classifyDic => TissuAnnotaionToColor(classifyDic["ownClassifyJRip"]);
+            using (var heatMap = tissueSlidePartitioner.GenerateHeatMap(drawWekaClassificationBaggingFunc))
+            {
+                slideCache.SetImage("ClassificationDetection_wekaClassifyBagging", heatMap);
+            }
+            using (var heatMap = tissueSlidePartitioner.GenerateHeatMap(drawOwnClassificationJ48Func))
+            {
+                slideCache.SetImage("ClassificationDetection_ownClassifyJRip", heatMap);
+            }
+            using (var heatMap = tissueSlidePartitioner.GenerateHeatMap(drawOwnClassificationJRipFunc))
+            {
+                slideCache.SetImage("ClassificationDetection_ownClassifyJRip", heatMap);
+            }
+        }
+
+        private static void DrawHeatMapOneClass(SlidePartitioner<Dictionary<String, TissuAnnotaionEnum>> tissueSlidePartitioner, SlideCache slideCache, TissuAnnotaionEnum annotaion)
         {
             
         }
 
+        private static Color TissuAnnotaionToColor(TissuAnnotaionEnum annotaion)
+        {
+            switch (annotaion)
+            {
+                case TissuAnnotaionEnum.Fettgewebe:
+                    return Color.White;
+                case TissuAnnotaionEnum.Tumor:
+                    return Color.Red;
+                case TissuAnnotaionEnum.Entzuendung:
+                    return Color.Aqua;
+                case TissuAnnotaionEnum.Gefaess:
+                    return Color.Green;
+                case TissuAnnotaionEnum.Mikrokalk:
+                    return Color.BlueViolet;
+                case TissuAnnotaionEnum.Nerv:
+                    return Color.SaddleBrown;
+                case TissuAnnotaionEnum.Stroma:
+                    return Color.Yellow;
+                case TissuAnnotaionEnum.DCIC:
+                    return Color.SpringGreen;
+                case TissuAnnotaionEnum.Kalk:
+                    return Color.Tan;
+                case TissuAnnotaionEnum.NormalesMammaepithel:
+                    return Color.DarkOrange;
+                case TissuAnnotaionEnum.NoTissue:
+                    return Color.Gray;
+                case TissuAnnotaionEnum.UnknowClass:
+                    return Color.Black;
+                default:
+                    return Color.Black;
+            }
+        }
     }
 
 }
